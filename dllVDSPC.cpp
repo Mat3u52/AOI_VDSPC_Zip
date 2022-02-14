1068e21bd2c1b5a3fc9dfc4dbec89e8ff95642b5
Mateusz <91789211+Mat3u52@users.noreply.github.com>
2022-02-14 19:28:45 +0100

    Update dllVDSPC.cpp && .h
    A few modifications.

--- dllVDSPC.cpp
+++ dllVDSPC.cpp
@@ -3,7 +3,10 @@
 #include <windows.h>
 #include <string>
 #include <iostream>
+#include <filesystem>
+
 using namespace std;
+namespace fs = filesystem;
 
 DllClass::DllClass(const string &s, const string &d){
     if(s.length() > 0 && d.length() > 0){
@@ -37,27 +40,35 @@ bool DllClass::pathValidatorDestination(void){
     }
 return false;
 }
-void DllClass::cleanUp(const string &y, const string &m){
+string DllClass::cleanUp(const string &y, const string &m){
     int month = stoi(m);
     int year = stoi(y);
 
-    switch(month){
-    case 1:
-        year =- 1;
-        char yearStr[10];
-        itoa(year,yearStr,10);
-        cout << yearStr << " " << "12" << endl;
-        break;
-    case 2:
-        month =- 1;
+    if(month > 1){
+        month -= 1;
         char monthStr[10];
         itoa(month,monthStr,10);
-        cout << y << " " << monthStr << endl;
-        break;
-    case 3:
-        cout << y << " " << m << endl;
-        break;
+        if(month < 10){
+            //cout << y + " " + "0"+monthStr << endl;
+            string yearMonth = y+"\\0"+monthStr;
+            return yearMonth;
+        }else{
+            //cout << y + " " +monthStr << endl;
+            string yearMonth = y+"\\"+monthStr;
+            return yearMonth;
+        }
+
+    }else{
+        year -= 1;
+        char yearStr[10];
+        itoa(year,yearStr,10);
+        //string monthStr = "12";
+        //cout << yearStr << " " << monthStr << endl;
+        string yearMonth = yearStr;
+        yearMonth += "\\12";
+        return yearMonth;
     }
+    return 0;
 }
 
 BOOL APIENTRY DllMain(HINSTANCE hInst, DWORD reason, LPVOID reserved){
--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ namespace fs = filesystem;
 int main(){
     string year = "";
     string mounth = "";
+    const string IPS = "\\\\tcznt100\\VITROX_AOI_BACKUP\\vdspc_image\\";
+    const string IPD = "\\\\tcznt100\\VITROX_AOI_ZIPPED_FILES\\vdspc_image\\";
 
     cout<<"[step 1.] Please, provide year:";
     cin>>year;
@@ -17,13 +19,13 @@ int main(){
     cin>>mounth;
 
     //string path = year+"\\"+mounth;
-    string pathSource = "\\\\tcznt100\\VITROX_AOI_BACKUP\\vdspc_image\\"+year+"\\"+mounth; /// source
+    //string pathSource = "\\\\tcznt100\\VITROX_AOI_BACKUP\\vdspc_image\\"+year+"\\"+mounth; /// source
+    string pathSource = IPS+""+year+"\\"+mounth; /// source
 
-    string pathDestination = "\\\\tcznt100\\VITROX_AOI_ZIPPED_FILES\\vdspc_image\\"+year+"\\"+mounth; /// destination
+    string pathDestination = IPD+""+year+"\\"+mounth; /// destination
 
     DllClass objectVDSPC(pathSource, pathDestination);
 
-    objectVDSPC.cleanUp(year, mounth);
     //if(pathValidator(pathSource) == true){
     if(objectVDSPC.pathValidatorSource() == true){
         //for(const auto& entry : fs::directory_iterator(year+"\\"+mounth+"\\")){ ///source
@@ -37,7 +39,8 @@ int main(){
                     fs::create_directory(pathDestination);
                 }
                 //string pathSnD = "powershell Compress-Archive -Path \""+year+"\\"+mounth+"\\"+filenameStr+"\" -DestinationPath \""+year+"\\"+mounth+"\\"+filenameStr+".zip\"";
-                string pathSnD = "powershell Compress-Archive -Path \"\\\\tcznt100\\VITROX_AOI_BACKUP\\vdspc_image\\"+year+"\\"+mounth+"\\"+filenameStr+"\" -DestinationPath \"\\\\tcznt100\\VITROX_AOI_ZIPPED_FILES\\vdspc_image\\"+year+"\\"+mounth+"\\"+filenameStr+".zip\"";
+                //string pathSnD = "powershell Compress-Archive -Path \"\\\\tcznt100\\VITROX_AOI_BACKUP\\vdspc_image\\"+year+"\\"+mounth+"\\"+filenameStr+"\" -DestinationPath \"\\\\tcznt100\\VITROX_AOI_ZIPPED_FILES\\vdspc_image\\"+year+"\\"+mounth+"\\"+filenameStr+".zip\"";
+                string pathSnD = "powershell Compress-Archive -Path \""+IPS+""+year+"\\"+mounth+"\\"+filenameStr+"\" -DestinationPath \""+IPD+""+year+"\\"+mounth+"\\"+filenameStr+".zip\"";
                 cout<<pathSnD<<endl;
                 system(pathSnD.c_str());
             }else if (entry.is_regular_file()){
@@ -46,6 +49,36 @@ int main(){
                 cout<<"?? "<<filenameStr<<endl;
             }
         }
+    }
+
+    string pathSourceRemove = IPS+""+objectVDSPC.cleanUp(year, mounth); /// source
+
+    string pathDestinationRemove = IPD+""+objectVDSPC.cleanUp(year, mounth); /// destination (not important)
+    //if(pathValidator(pathSource) == true){
+    //if(objectVDSPC.pathValidatorSource() == true){
+    cout << pathSourceRemove << endl;
+    //cout << pathDestinationRemove << endl;
+    DllClass objectVDSPCRemove(pathSourceRemove, pathDestinationRemove);
+    if(objectVDSPCRemove.pathValidatorSource() == true){
+        cout << "Exist." << endl;
+        /*
+        for(const auto& entry : fs::directory_iterator(pathSourceRemove+"\\")){ ///source
+            const auto filenameStr = entry.path().filename().string();
+            if(entry.is_directory()){
+                cout<<"dir:  "<<filenameStr<<endl;
+
+                fs::remove_all("C:\\01\\01");
+
+
+            }else if (entry.is_regular_file()){
+                cout<<"file: "<<filenameStr<<endl;
+            }else{
+                cout<<"?? "<<filenameStr<<endl;
+            }
+        }
+        */
+    }else{
+        cout << "The path not exist." << endl;
     }
 
     return 0;
