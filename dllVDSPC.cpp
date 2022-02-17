dba0113c318110f06f6f409301f329fed870bebe
Mateusz <91789211+Mat3u52@users.noreply.github.com>
2022-02-17 20:39:45 +0100

    Update
    Sanitization of code.

--- dllVDSPC.cpp
+++ dllVDSPC.cpp
@@ -49,11 +49,9 @@ string DllClass::cleanUp(const string &y, const string &m){
         char monthStr[10];
         itoa(month,monthStr,10);
         if(month < 10){
-            //cout << y + " " + "0"+monthStr << endl;
             string yearMonth = y+"\\0"+monthStr;
             return yearMonth;
         }else{
-            //cout << y + " " +monthStr << endl;
             string yearMonth = y+"\\"+monthStr;
             return yearMonth;
         }
@@ -62,8 +60,6 @@ string DllClass::cleanUp(const string &y, const string &m){
         year -= 1;
         char yearStr[10];
         itoa(year,yearStr,10);
-        //string monthStr = "12";
-        //cout << yearStr << " " << monthStr << endl;
         string yearMonth = yearStr;
         yearMonth += "\\12";
         return yearMonth;
@@ -75,13 +71,10 @@ BOOL APIENTRY DllMain(HINSTANCE hInst, DWORD reason, LPVOID reserved){
     switch(reason){
         case DLL_PROCESS_ATTACH:
             break;
-
         case DLL_PROCESS_DETACH:
             break;
-
         case DLL_THREAD_ATTACH:
             break;
-
         case DLL_THREAD_DETACH:
             break;
     }
